4a09c9de1d9c638a8d400a6996728a0f5cf4c604
student <lpierrelouis17@mail.valenciacollege.edu>
2024-10-01 17:54:01 -0400

    Fourth Commit

--- second.py
+++ second.py
@@ -10,3 +10,15 @@ elif not a or b or not b and a:
     print(3)
 else:
     print(4)
+
+print('')
+
+def menu(choice):
+    var1 = 'Me'
+    var2 = 'You'
+    if(choice == 1):
+        return var1
+    else:
+        return var2
+
+print(menu(3))
